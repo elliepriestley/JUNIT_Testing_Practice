f6eb59d8edc44977a35e709ebad8487c2cbc5ce0
elliepriestley <ellie.priestley@gmail.com>
2024-02-01 15:34:13 +0000

    Used Nested Tests and DisplayName from junit library to order Fizzbuzz tests

--- src/main/kotlin/games/Fizzbuzz.kt
+++ src/main/kotlin/games/Fizzbuzz.kt
@@ -1,4 +1,20 @@
 package games
 
 class Fizzbuzz {
+    fun fizzbuzz(inputNum: Int): String {
+        return when {
+            inputNum % 3 == 0 && inputNum % 5 == 0 -> {
+                "Fizzbuzz"
+            }
+            inputNum % 5 == 0 -> {
+                "Buzz"
+            }
+            inputNum % 3 == 0 -> {
+                "Fizz"
+            }
+            else -> {
+                inputNum.toString()
+            }
+        }
+    }
 }
--- src/test/kotlin/games/FizzbuzzTest.kt
+++ src/test/kotlin/games/FizzbuzzTest.kt
@@ -1,5 +1,49 @@
 package games
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
-class FizzbuzzTest
+class FizzbuzzTest {
+
+    @Nested
+    @DisplayName("Unit Tests for Fizzbuzz application")
+    inner class UnitTest {
+        @Test fun `When input is 3 Output is Fizz`() {
+            val fizzbuzzApp = Fizzbuzz()
+            val actual = fizzbuzzApp.fizzbuzz(3)
+            assertEquals("Fizz", actual)
+        }
+
+        @Test fun `When input is 5 Output is Buzz`() {
+            val fizzbuzzApp = Fizzbuzz()
+            val actual = fizzbuzzApp.fizzbuzz(5)
+            assertEquals("Buzz", actual)
+        }
+
+        @Test fun `When input is 15 Output is Fizzbuzz`() {
+            val fizzbuzzApp = Fizzbuzz()
+            val actual = fizzbuzzApp.fizzbuzz(15)
+            assertEquals("Fizzbuzz", actual)
+        }
+
+        @Test fun `When input is anything else (num) Output is num`() {
+            val fizzbuzzApp = Fizzbuzz()
+            val actual = fizzbuzzApp.fizzbuzz(19)
+            assertEquals("19", actual)
+        }
+    }
+    @Nested
+    @DisplayName("Integration Tests for Fizzbuzz application")
+    inner class IntegrationTest {
+        @Test fun `Some integration test`() {
+            TODO()
+        }
+
+        @Test fun `Another integration test`() {
+            TODO()
+        }
+    }
+
+}
